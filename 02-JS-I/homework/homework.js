803e63f6d07257d46ae6cae2909a428670ed990c
Gaspar <jgalt268@gmail.com>
2022-03-04 21:36:36 -0300

    FI NA LI ZA DOOOOOUUUUUU

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -241,19 +241,18 @@ function deEuroAdolar(euro){
 
 
 function esVocal(letra){
-  const letra = "letra";
   //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
   //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
- if (letra.length > 1) {
+  if (letra.length > 1) {
    return "Dato incorrecto"
  } if (letra === "a"||
-                 "e"|| 
-                 "i"||
-                 "o"|| 
-                 "u" {
+       letra === "e"|| 
+       letra === "i"||
+       letra === "o"|| 
+       letra === "u") {
    return "Es vocal"
  } else {
    return "Dato incorrecto"
